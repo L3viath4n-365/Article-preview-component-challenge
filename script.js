ff9749ebfc6c62eb3d80a082ba76a605f12c819c
L3viath4n-365 <michaelekeoma01@gmail.com>
2026-06-21 11:47:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const shareBTN = $('[id="share-button"]');
+const mobileBar = $('#mobile-social-bar');
+const authorSec = $('#author-section');
+const desktopBar = $('#hidden-class');
+
+let touched = false;
+
+shareBTN.on('touchend', (e) => {
+    touched = true;
+    const isHidden = mobileBar.prop('hidden');
+
+    if (isHidden) {
+        authorSec.prop('hidden', true);
+        mobileBar.prop('hidden', false);
+    } else {
+        authorSec.prop('hidden', false);
+        mobileBar.prop('hidden', true);
+    }
+
+    e.preventDefault();
+});
+
+shareBTN.on('mouseover', () => {
+    if (touched) {
+        touched = false;
+        return;
+    }
+
+    desktopBar.toggle();
+});
